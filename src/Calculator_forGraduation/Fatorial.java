2861d441fadffc6663560fb2dd1d426240e1bf2a
Eduardo_Schulle <Eduardo.r.schulle@gmail.com>
2023-04-08 19:30:17 -0300

    alteracao no projeto

---
+++ src/Calculator_forGraduation/Fatorial.java
@@ -0,0 +1,16 @@
+package Calculator_forGraduation;
+
+public class Fatorial extends Operacao {
+
+	
+	public float calcula () {
+		int resultado = 1;
+	    for (int i = 2; i <= numero1; i++) {
+	        resultado *= i;
+		
+		
+	}
+	    
+	    return resultado;
+}
+}	
---
+++ src/Calculator_forGraduation/Potencia.java
@@ -0,0 +1,22 @@
+package Calculator_forGraduation;
+
+public class Potencia extends Operacao {
+
+	
+	public double calcula (){
+		
+		
+		 double resultado = 1;
+		    for (int i = 0; i < numero1; i++) {
+		        resultado *= i;	
+		
+	}
+		    
+		    
+		    return resultado;
+	
+	
+}
+	
+	
+}
